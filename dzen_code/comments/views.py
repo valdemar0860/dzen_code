4735952557c9eb2d9c8ab4ce3e3f42cb3977c49e
Volodymyr Shargorodskiy <gsmkibernetik@gmail.com>
2024-01-13 10:07:24 +0200

    edit dockerfile and comments/views.py

--- dzen_code/comments/views.py
+++ dzen_code/comments/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
@@ -10,14 +10,14 @@ class CommentPagination(PageNumberPagination):
     page_size = 25
 
 
-class CommentListCreateView(generics.ListCreateAPIView):
+class CommentListCreateView(ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = Comment.objects.filter(parent_comment__isnull=True).order_by('-date_added')
     serializer_class = CommentSerializer
     pagination_class = CommentPagination
 
 
-class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+class CommentDetailView(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
